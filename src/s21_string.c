327f9e5ff891a980e2536f66592ea2aba0b057b9
Tania Kiara <taniakia@ge-n3.kzn.21-school.ru>
2022-11-18 13:08:58 +0300

    implemented s21_memcmp func

--- src/s21_string.c
+++ src/s21_string.c
@@ -15,8 +15,11 @@ void *s21_memchr(const void *str, int c, s21_size_t n) {
     2. Compares the first n bytes of str1 and str2.
 **/
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
-  (void)str1;
-  (void)str2;
-  (void)n;
-  return 0;
+  int result = 0;
+  for (s21_size_t i = 0; i < n && !result; i++) {
+    unsigned char ch1 = *(unsigned char *)(str1 + i);
+    unsigned char ch2 = *(unsigned char *)(str2 + i);
+    if (ch1 != ch2) result = ch1 - ch2;
+  }
+  return result;
 }
