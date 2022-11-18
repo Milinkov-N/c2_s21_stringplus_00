63d5a92719c213c45620a60f0a3f56573804b424
Tania Kiara <taniakia@ge-n3.kzn.21-school.ru>
2022-11-18 12:58:10 +0300

    fixed build issues

--- src/s21_string.c
+++ src/s21_string.c
@@ -14,4 +14,9 @@ void *s21_memchr(const void *str, int c, s21_size_t n) {
 /**
     2. Compares the first n bytes of str1 and str2.
 **/
-int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {}
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
+  (void)str1;
+  (void)str2;
+  (void)n;
+  return 0;
+}
