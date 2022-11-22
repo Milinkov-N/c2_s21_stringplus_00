59da3ae3325ec108010d4f188eb8310124e63273
Kolya Davydov <kolya.davydov@inbox.ru>
2022-11-22 23:33:17 +0300

    added 11-19 (function and tests) for string.c

--- src/s21_string.c
+++ src/s21_string.c
@@ -191,3 +191,207 @@ int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
   }
   return result;
 }
+
+/**
+    11. Copies the string pointed to, by src to dest.
+
+    Implementes by: Almeta Terry
+**/
+char *s21_strcpy(char *dest, const char *src) {
+  char *tmp = dest;
+  while(*src != '\0') {
+    *dest++ = *src++;
+  }
+  *dest = '\0';
+  return tmp;
+}
+
+/**
+    12. Copies up to n characters from the string pointed to, by src to dest.
+
+    Implementes by: Almeta Terry
+**/
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
+  char *tmp = dest;
+  s21_size_t tmp_n = 0;
+  while(tmp_n < n) {
+    if (*src != '\0') {
+      *dest++ = *src++;
+    } else {
+      *dest++ = '\0';
+    }
+    tmp_n++;
+  }
+  return tmp;
+}
+
+/**
+    13. Calculates the length of the initial segment of str1 which consists
+    entirely of characters not in str2.
+
+    Implementes by: Almeta Terry
+**/
+s21_size_t s21_strcspn(const char *str1, const char *str2) {
+  s21_size_t counter = 0;
+  while(*str1 != '\0' && !s21_strchr(str2, *str1++)) counter++;
+  return counter;
+}
+
+/**
+    14. Searches an internal array for the error number errnum and returns a
+    pointer to an error message string. You need to declare macros containing
+    arrays of error messages for mac and linux operating systems. Error
+    descriptions are available in the original library. Checking the current OS
+    is carried out using directives.
+
+    Implementes by: Almeta Terry
+**/
+
+// макросы (ERROR_NUM и LIST_ERROR) содержащие массивы строк в зависимости от ОС (linux  или mac)
+// объявлены в заголовочном файле, там же с помощью деректив
+// определяется текущая операционная система
+
+char *s21_strerror(int errnum) {
+  static char error[1024] = {'\0'};
+  char *error_list[] = LIST_ERROR;
+  if(errnum >= 0 && errnum < ERROR_NUM) {
+    s21_strcpy(error, error_list[errnum]);
+  } else {
+    char tmp_num[50] = {0}; // для хранения числа в виде строки
+#if defined (__linux__)
+  s21_strcpy(error, "Unknown error ");
+#elif (__APPLE__)
+  s21_strcpy(error, "Unknown error: ");
+#endif
+// ЛИБО ЗДЕСЬ МОЖНО БУДЕТ ВОСПОЛЬЗОТЬСЯ ФУНКЦИЕЙ S21_sprintf()
+  s21_strcat(error, int_to_str(tmp_num, errnum));
+  }
+  return error;
+}
+
+// ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ, ЕСЛИ СДЛЕАЕМ sprintf, то можно сократить
+// ALMETA TERRY
+char *int_to_str(char *tmp_num, int errnum) {
+  int num = errnum;
+  int pow_of_ten = 1;
+  int minus = 0; // если число отрицаткльное то будет = 1
+  if (num < 0) {
+    minus = 1;
+    num = -num;
+  }
+  if (num == 0) {
+    tmp_num[0] = '0';
+    tmp_num[1] = '\0';
+  } else {
+    while (pow_of_ten <= num) {
+      pow_of_ten *= 10;
+    }
+    pow_of_ten /= 10;
+    int position; // разряд числа
+    if (minus == 0) {
+      position = 0;
+    } else {
+      tmp_num[0] = '-';
+      position = 1;
+    }
+    while (pow_of_ten != 0) {
+      tmp_num[position] = '0' + num / pow_of_ten;
+      num = num - num / pow_of_ten * pow_of_ten;
+      pow_of_ten /= 10;
+      position++;
+    }
+    tmp_num[position] = '\0';
+  }
+  return tmp_num;
+}
+
+/**
+    15. Computes the length of the string str up to but not including the
+    terminating null character.
+
+    Implementes by: Almeta Terry
+**/
+s21_size_t s21_strlen(const char *str) {
+  s21_size_t length = 0;
+  if(str != s21_NULL) {
+    while(*str != 0) {
+      length++;
+      str++;
+    }
+  }
+  return length;
+}
+
+/**
+    16. Finds the first character in the string str1 that matches any character
+    specified in str2.
+
+    Implementes by: Almeta Terry
+**/
+char *s21_strpbrk(const char *str1, const char *str2) {
+  char *ptr = s21_NULL;
+  int flag = 0;
+  while(*str1 != '\0' && flag == 0) {
+    for (s21_size_t i = 0; i < s21_strlen(str2); i++) {
+      if(*str1 == str2[i]) {
+        flag = 1;
+        ptr = (char *)str1;
+      }
+    }
+    str1++;
+  }
+  return ptr;
+}
+
+/**
+    17. Searches for the last occurrence of the character c (an unsigned char)
+    in the string pointed to by the argument str.
+
+    Implementes by: Almeta Terry
+**/
+char *s21_strrchr(const char *str, int c) {
+  char *ptr = s21_NULL;
+  for(s21_size_t i = 0; i < s21_strlen(str) + 1; i++) {
+    if(str[i] == (unsigned char) c)
+      ptr = (char *) &str[i];
+  }
+  return ptr;
+}
+
+/**
+    18. Calculates the length of the initial segment of str1 which consists
+    entirely of characters in str2.
+
+    Implementes by: Almeta Terry
+**/
+s21_size_t s21_strspn(const char *str1, const char *str2) {
+  s21_size_t counter = 0;
+  while (*str1 != '\0' && s21_strchr(str2, *str1++)) {
+    counter++;
+  }
+  return counter;
+}
+
+/**
+    19. Finds the first occurrence of the entire string needle (not including
+    the terminating null character) which appears in the string haystack.
+
+    Implementes by: Almeta Terry
+**/
+char *s21_strstr(const char *haystack, const char *needle) {
+  s21_size_t i = 0, j = 0;
+  char *ptr = s21_NULL;
+  if (needle[0] == '\0') {
+    ptr = (char *)haystack;
+  }
+  while (haystack[i] != '\0') {
+    while (haystack[i + j] != '\0' && haystack[i + j] == needle[j]) {
+      if (needle[j + 1] == '\0') {
+        ptr = (char *)(haystack + i);
+      }
+      ++j;
+    }
+    ++i;
+  }
+  return ptr;
+}
--- src/tests.c
+++ src/tests.c
@@ -294,6 +294,223 @@ START_TEST(tc34_strncmp) {
 }
 END_TEST
 
+// 11. Тесты для strcpy
+
+START_TEST(tc35_strcpy) {
+  char dest1[30] = "S21_SCHOOL";
+  char dest2[30] = "S21_SCHOOL";
+  char str[30] = "Is back!";
+  ck_assert_str_eq(strcpy(dest1, str), s21_strcpy(dest2, str));
+}
+END_TEST
+
+START_TEST(tc36_strcpy) {
+  char dest1[30] = "S21_SCHOOL";
+  char dest2[30] = "S21_SCHOOL";
+  char str[30] = "Is back! Is hard....";
+  ck_assert_str_eq(strcpy(dest1, str), s21_strcpy(dest2, str));
+}
+END_TEST
+
+START_TEST(tc37_strcpy) {
+  char dest1[30] = "";
+  char dest2[30] = "";
+  char str[30] = "Is back! Is hard....";
+  ck_assert_str_eq(strcpy(dest1, str), s21_strcpy(dest2, str));
+}
+END_TEST
+
+// 12. Тесты для strncpy
+
+START_TEST(tc38_strncpy) {
+  s21_size_t n = 9;
+  char dest1[30] = "S21_SCHOOL";
+  char dest2[30] = "S21_SCHOOL";
+  char str[30] = "Is back!";
+  ck_assert_str_eq(strncpy(dest1, str, n), s21_strncpy(dest2, str, n));
+}
+END_TEST
+
+START_TEST(tc39_strncpy) {
+  s21_size_t n = 1;
+  char dest1[30] = "S21_SCHOOL";
+  char dest2[30] = "S21_SCHOOL";
+  char str[30] = "Is back!";
+  ck_assert_str_eq(strncpy(dest1, str, n), s21_strncpy(dest2, str, n));
+}
+END_TEST
+
+START_TEST(tc40_strncpy) {
+  s21_size_t n = 5;
+  char dest1[30] = "";
+  char dest2[30] = "";
+  char str[30] = "Is back!";
+  ck_assert_str_eq(strncpy(dest1, str, n), s21_strncpy(dest2, str, n));
+}
+END_TEST
+
+// 13. Тесты для strcspn
+
+START_TEST(tc41_strcspn) {
+  char str1[30] = "S21_SCHOOL";
+  char str2[30] = "S21_SCHOOL";
+  ck_assert_int_eq(strcspn(str1,str2), s21_strcspn(str1, str2));
+}
+END_TEST
+
+START_TEST(tc42_strcspn) {
+  char str1[30] = "S21_SCHOOL";
+  char str2[30] = "L";
+  ck_assert_int_eq(strcspn(str1,str2), s21_strcspn(str1, str2));
+}
+END_TEST
+
+START_TEST(tc43_strcspn) {
+  char str1[30] = "S21_SCHOOL";
+  char str2[30] = "ak47";
+  ck_assert_int_eq(strcspn(str1,str2), s21_strcspn(str1, str2));
+}
+END_TEST
+
+// 14. Тесты для strerror
+
+START_TEST(tc44_strerror) {
+  int err = 0;
+  ck_assert_str_eq(strerror(err), s21_strerror(err));
+}
+END_TEST
+
+START_TEST(tc45_strerror) {
+  int err = 60;
+  ck_assert_str_eq(strerror(err), s21_strerror(err));
+}
+END_TEST
+
+START_TEST(tc46_strerror) {
+  int err = 300;
+  ck_assert_str_eq(strerror(err), s21_strerror(err));
+}
+END_TEST
+
+START_TEST(tc47_strerror) {
+  int err = -1000;
+  ck_assert_str_eq(strerror(err), s21_strerror(err));
+}
+END_TEST
+
+// 15. Тесты для strlen
+
+START_TEST(tc48_strlen) {
+  char str[100] = ""; 
+  ck_assert_int_eq(strlen(str), s21_strlen(str));
+}
+END_TEST
+
+START_TEST(tc49_strlen) {
+  char str[100] = "Hello world!"; 
+  ck_assert_int_eq(strlen(str), s21_strlen(str));
+}
+END_TEST
+
+START_TEST(tc50_strlen) {
+  char str[100] = "Hello \0\t\nworld!"; 
+  ck_assert_int_eq(strlen(str), s21_strlen(str));
+}
+END_TEST
+
+// 16. Тесты для strpbrk
+
+START_TEST(tc51_strpbrk) {
+  char str1[] = "Hello world";
+  char str2[] = "W";
+  ck_assert_pstr_eq(s21_strpbrk(str1, str2), strpbrk(str1, str2));
+}
+END_TEST
+
+START_TEST(tc52_strpbrk) {
+  char str1[] = "HELLO WORLD";
+  char str2[] = "C";
+  ck_assert_pstr_eq(s21_strpbrk(str1, str2), strpbrk(str1, str2));
+}
+END_TEST
+
+START_TEST(tc53_strpbrk) {
+  char str1[] = "";
+  char str2[] = "";
+  ck_assert_pstr_eq(s21_strpbrk(str1, str2), strpbrk(str1, str2));
+}
+END_TEST
+
+// 17. Тесты для strrchr
+
+START_TEST(tc54_strrchr) {
+  char str[] = "Hello world";
+  char ch = 'o';
+  ck_assert_pstr_eq(s21_strrchr(str, ch), strrchr(str, ch));
+}
+END_TEST
+
+START_TEST(tc55_strrchr) {
+  char str[] = "Hello world";
+  char ch = '\0';
+  ck_assert_pstr_eq(s21_strrchr(str, ch), strrchr(str, ch));
+}
+END_TEST
+
+START_TEST(tc56_strrchr) {
+  char str[] = "Hello world";
+  char ch = 'W';
+  ck_assert_pstr_eq(s21_strrchr(str, ch), strrchr(str, ch));
+}
+END_TEST
+
+// 18. Тесты для strrspn
+
+START_TEST(tc57_strspn) {
+  char str1[] = "";
+  char str2[] = "";
+  ck_assert_int_eq(s21_strspn(str1, str2), strspn(str1, str2));
+}
+END_TEST
+
+START_TEST(tc58_strspn) {
+  char str1[] = "Hello world!";
+  char str2[] = "";
+  ck_assert_int_eq(s21_strspn(str1, str2), strspn(str1, str2));
+}
+END_TEST
+
+START_TEST(tc59_strspn) {
+  char str1[] = "Hi";
+  char str2[] = "HiHi";
+  ck_assert_int_eq(s21_strspn(str1, str2), strspn(str1, str2));
+}
+END_TEST
+
+// 19. Тесты для strstr
+
+START_TEST(tc60_strstr) {
+  char str1[] = "HELLO world";
+  char str2[] = "world";
+  ck_assert_pstr_eq(s21_strstr(str1, str2), strstr(str1, str2));
+}
+END_TEST
+
+START_TEST(tc61_strstr) {
+  char str1[] = "Hello world";
+  char str2[] = "world ";
+  ck_assert_pstr_eq(s21_strstr(str1, str2), strstr(str1, str2));
+}
+END_TEST
+
+START_TEST(tc62_strstr) {
+  char str1[] = "Hello world";
+  char str2[] = "";
+  ck_assert_pstr_eq(s21_strstr(str1, str2), strstr(str1, str2));
+}
+END_TEST
+
+
 Suite *ts_s21_suite_insert(void) {
   Suite *suite = suite_create("ts_s21_suite_insert");
   TCase *test_case = tcase_create("tc_s21_insert");
@@ -342,6 +559,42 @@ Suite *ts_s21_suite_insert(void) {
   tcase_add_test(test_case, tc33_strncmp);
   tcase_add_test(test_case, tc34_strncmp);
 
+  tcase_add_test(test_case, tc35_strcpy);
+  tcase_add_test(test_case, tc36_strcpy);
+  tcase_add_test(test_case, tc37_strcpy);
+
+  tcase_add_test(test_case, tc38_strncpy);
+  tcase_add_test(test_case, tc39_strncpy);
+  tcase_add_test(test_case, tc40_strncpy);
+
+  tcase_add_test(test_case, tc41_strcspn);
+  tcase_add_test(test_case, tc42_strcspn);
+  tcase_add_test(test_case, tc43_strcspn);
+
+  tcase_add_test(test_case, tc44_strerror);
+  tcase_add_test(test_case, tc45_strerror);
+  tcase_add_test(test_case, tc46_strerror);
+  tcase_add_test(test_case, tc47_strerror);
+
+  tcase_add_test(test_case, tc48_strlen);
+  tcase_add_test(test_case, tc49_strlen);
+  tcase_add_test(test_case, tc50_strlen);
+  
+  tcase_add_test(test_case, tc51_strpbrk);
+  tcase_add_test(test_case, tc52_strpbrk);
+  tcase_add_test(test_case, tc53_strpbrk);
+
+  tcase_add_test(test_case, tc54_strrchr);
+  tcase_add_test(test_case, tc55_strrchr);
+  tcase_add_test(test_case, tc56_strrchr);
+
+  tcase_add_test(test_case, tc57_strspn);
+  tcase_add_test(test_case, tc58_strspn);
+  tcase_add_test(test_case, tc59_strspn);
+
+  tcase_add_test(test_case, tc60_strstr);
+  tcase_add_test(test_case, tc61_strstr);
+  tcase_add_test(test_case, tc62_strstr);
 
   suite_add_tcase(suite, test_case);
 
